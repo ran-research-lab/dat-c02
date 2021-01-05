afcc52cf434917cb8e19f8698e02a9d15bf5001a
Rafael A. Arce Nazario <rafael.arce@upr.edu>
2021-01-05 16:07:45 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() 
+#include "catch_amalgamated.hpp"
+#include <vector>
+
+using namespace std;
+
+
+int highestFrequency( const vector<int> &v ) {
+    return 0;
+}
+
+TEST_CASE( "Testing the highestFrequency() function", "[highestFrequency]" ) {
+    //REQUIRE( highestFrequency(vector<int> {100, 45, 98, 45, 78, 45})  == 3);
+}
